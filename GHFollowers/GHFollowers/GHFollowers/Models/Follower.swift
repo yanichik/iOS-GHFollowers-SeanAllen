8423eeaf951f1a034a2388080e2ad6e0bfbfd5b7
Yan <brunshte@gmail.com>
2024-03-26 18:12:15 -0700

    start tracking GHFollowers

---
+++ GHFollowers/GHFollowers/GHFollowers/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by YB on 3/26/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
