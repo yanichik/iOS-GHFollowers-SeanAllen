6cb3c2744a50ebe67c471db44029f8cb9a735960
Yan <brunshte@gmail.com>
2024-03-26 18:16:44 -0700

    fix directory reorg

---
+++ GHFollowers/GHFollowers/GHFollowers/Custom Views/GFBodyLabel.swift
@@ -0,0 +1,33 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowers
+//
+//  Created by YB on 3/19/24.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        font = UIFont.preferredFont(forTextStyle: .body) 
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GHFollowers/GHFollowers/GHFollowers/Custom Views/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by YB on 3/16/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    // storyboard init Req'd by Apple but not used when no storyboard avail
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configureUI()
+    }
+    
+    private func configureUI() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline) // enables dynamic font
+        translatesAutoresizingMaskIntoConstraints = false // sets to use autolayout instead of storyboard
+    }
+}
